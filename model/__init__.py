dadb91999af194757eb34ca0eedb05ef979e8a09
Thiemo Frei <thiemo.frei@hotmail.com>
2025-04-23 18:13:47 +0200

    Update __init__.py

--- model/__init__.py
+++ model/__init__.py
@@ -1,1 +1,8 @@
-
+from .room import Room
+from .room_type import Room_Type
+from .hotel import Hotel
+from .room_facility import Room_Facility
+from .booking import Booking
+from .guest import Guest
+from .address import Address
+from .invoice import Invoice
