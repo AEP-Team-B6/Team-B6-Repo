3071cf4fa6fa0e2b1a8eb8519f4f0e9a30480646
Thiemo Frei <thiemo.frei@hotmail.com>
2025-04-23 18:17:30 +0200

    Create room_type.py

---
+++ model/room_type.py
@@ -0,0 +1,30 @@
+#Creating class Room_Type
+class Room_Type:
+    def __init__(self, type_id:int, description:str, max_guests:int):
+        self.__type_id = type_id
+        self.__description = description
+        self.__max_guests = max_guests
+
+    @property
+    def type_id(self):
+        return self.__type_id
+
+    @type_id.setter
+    def type_id(self, new_type_id):
+        self.__type_id = new_type_id
+
+    @property
+    def description(self):
+        return self.__description
+
+    @description.setter
+    def description(self, new_description):
+        self.__description = new_description
+
+    @property
+    def max_guests(self):
+        return self.__max_guests
+
+    @max_guests.setter
+    def max_guests(self, new_max_guests):
+        self.__max_guests = new_max_guests
