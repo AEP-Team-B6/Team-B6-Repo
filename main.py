e1ca258127286b481732935bab14bfff373efa26
Nils Strehle <nils.strehle@hotmail.com>
2025-05-29 11:35:11 +0200

    Data source in main.py corrected with new db file

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import shutil
+import os
 
-working_db = "./database/working_db.db"
+hotel_reservation_sample_db = "./database/hotel_reservation_sample.db"
 current_db = "./database/current_db.db"
 
-shutil.copyfile(working_db, current_db)
+shutil.copyfile(hotel_reservation_sample_db, current_db)
 
 os.environ["DB_FILE"] = current_db
 
