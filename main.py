77a1029890c87d04cbc5e2f053dd33f472a48210
Thiemo Frei <thiemo.frei@hotmail.com>
2025-05-29 13:59:20 +0200

    Update main with testing space

--- main.py
+++ main.py
@@ -9,3 +9,21 @@ shutil.copyfile(hotel_reservation_sample_db, current_db)
 os.environ["DB_FILE"] = current_db
 
 #TODO: Add more stuff
+
+
+# Individuelle Testing -------------------
+
+While False:
+    # Testbereich (auf True sezten zum Testen)    
+
+
+
+
+
+
+
+
+
+# Funktionierender Code------------------------
+While True:
+    #Funktionierende Userstories (auf False sezten zum Testen)
